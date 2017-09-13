79280fd70f57baf3e3bca2cb0a374e5f6d308a3f
JunaidZaheer <30520021+JunaidZaheer@users.noreply.github.com>
2017-09-13 05:05:10 -0700

    Update ShoppingCartController.cs

--- ShoppingCart/Controllers/ShoppingCartController.cs
+++ ShoppingCart/Controllers/ShoppingCartController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -65,7 +65,8 @@ namespace ShoppingCartSPA1.Controllers
         [Route("api/GetCart")]
         public ShoppingCart GetCart()
         {
-            return null;//TODO HERE!
+
+            return HttpContext.Current.Session["sc"] as ShoppingCart;
         }
     }
 }
